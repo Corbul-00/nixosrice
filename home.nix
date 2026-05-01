ede4026a5169584f941630826be09d75c82a3cb6
Corbul-00 <corbul_00@pm.me>
2026-05-01 19:01:36 -0300

    PFtchD

--- home.nix
+++ home.nix
@@ -6,17 +6,17 @@
   
  
   imports = [
-    ./home/modules/kitty/pink.nix
-    ./home/modules/hyprland/pink.nix
-    ./home/modules/waybar/pink.nix
+    ./home/modules/kitty/default.nix
+    ./home/modules/hyprland/default.nix
+    ./home/modules/waybar/default.nix
     ./home/modules/fish/default.nix
-    ./home/modules/gtk/pink.nix # Comment: Added global GTK theme configuration.
-    ./home/modules/fastfetch/pink.nix
+    ./home/modules/gtk/default.nix # Comment: Added global GTK theme configuration.
+    ./home/modules/fastfetch/default.nix
     ./home/modules/btop/default.nix
     ./home/modules/librewolf/default.nix
     ./home/modules/lazyvim/default.nix
     ./home/modules/mangohud/default.nix
-    ./home/modules/yazi/pink.nix
+    ./home/modules/yazi/default.nix
   ]; 
 
 
