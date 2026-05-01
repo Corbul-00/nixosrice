a82ddb65b34942ca01087519d0a0a2c27daae0d0
Corbul-00 <corbul_00@pm.me>
2026-05-01 12:13:08 -0300

    DBAdd

--- home.nix
+++ home.nix
@@ -35,6 +35,23 @@
     enable = true;
     defaultEditor = true;
   };
+  #Default Browser
+  
+  home.sessionVariables = {
+    DEFAULT_BROWSER = "${pkgs.firefox}/bin/firefox";
+    BROWSER = "${pkgs.firefox}/bin/firefox";
+  };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = [ "firefox.desktop" ];
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "x-scheme-handler/about" = [ "firefox.desktop" ];
+      "x-scheme-handler/unknown" = [ "firefox.desktop" ];
+    };
+  };
 
   #Hyprland
   #programs.waybar.enable = true;
