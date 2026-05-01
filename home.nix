212d58ac7cbe51c62f6e7ddb3bdce4e3d700e616
Corbul-00 <corbul_00@pm.me>
2026-05-01 18:25:36 -0300

    yazipt

--- home.nix
+++ home.nix
@@ -6,11 +6,11 @@
   
  
   imports = [
-    ./home/modules/kitty/default.nix
-    ./home/modules/hyprland/default.nix
-    ./home/modules/waybar/default.nix
+    ./home/modules/kitty/pink.nix
+    ./home/modules/hyprland/pink.nix
+    ./home/modules/waybar/pink.nix
     ./home/modules/fish/default.nix
-    ./home/modules/gtk/default.nix # Comment: Added global GTK theme configuration.
+    ./home/modules/gtk/pink.nix # Comment: Added global GTK theme configuration.
     ./home/modules/fastfetch/default.nix
     ./home/modules/btop/default.nix
     ./home/modules/librewolf/default.nix
