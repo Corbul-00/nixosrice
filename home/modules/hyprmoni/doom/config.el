9279e72cdf9bb436f9a6bca10400d877484cbe56
Corbul-00 <corbul_00@pm.me>
2026-08-14 19:29:38 -0300

    doomT4

---
+++ home/modules/hyprmoni/doom/config.el
@@ -0,0 +1,11 @@
+(setq doom-theme 'doom-one)
+
+(setq doom-font
+      (font-spec
+       :family "JetBrainsMono Nerd Font"
+       :size 12))
+
+(setq display-line-numbers-type 'relative)
+
+(setq fancy-splash-image
+      (expand-file-name "emacsbg.png" doom-private-dir))
---
+++ home/modules/hyprmoni/doom/init.el
@@ -0,0 +1,32 @@
+(doom!
+ :completion
+ vertico
+ corfu
+
+ :ui
+ doom
+ doom-dashboard
+ modeline
+ indent-guides
+
+ :editor
+ (evil +everywhere)
+ snippets
+
+ :emacs
+ dired
+ undo
+ vc
+
+ :tools
+ lsp
+ magit
+
+ :lang
+ nix
+ python
+ javascript
+ rust
+
+ :config
+ (default +bindings))
