d98f14147609921f9ffdfd0f235d51e5875439ac
Corbul-00 <corbul_00@pm.me>
2026-08-28 18:11:54 -0300

    rg-cli2

--- home/modules/rg-cli/default.nix
+++ home/modules/rg-cli/default.nix
@@ -1,23 +1,27 @@
 { pkgs, ... }:
 
-pkgs.stdenv.mkDerivation {
-  pname = "rg-cli";
-  version = "0.1.0";
+{
+  home.packages = [
+    (pkgs.stdenv.mkDerivation {
+      pname = "rg-cli";
+      version = "0.1.0";
 
-  src = ./rg-cli;
+      src = ./rg-cli;
 
-  dontUnpack = true;
-  dontConfigure = true;
-  dontBuild = true;
+      dontUnpack = true;
+      dontConfigure = true;
+      dontBuild = true;
 
-  installPhase = ''
-    mkdir -p $out/bin
-    cp $src $out/bin/rg-cli
-    chmod +x $out/bin/rg-cli
-  '';
+      installPhase = ''
+        mkdir -p $out/bin
+        cp $src $out/bin/rg-cli
+        chmod +x $out/bin/rg-cli
+      '';
 
-  meta = {
-    description = "Fast, lightweight RedGIFs search, batch downloader, and mpv player CLI";
-    mainProgram = "rg-cli";
-  };
+      meta = {
+        description = "Fast, lightweight RedGIFs search, batch downloader, and mpv player CLI";
+        mainProgram = "rg-cli";
+      };
+    })
+  ];
 }
